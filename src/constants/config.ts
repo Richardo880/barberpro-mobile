55201a26352c02b3fdb83eb0e3728610fdb88b4a
Richardo880 <ricardotoledo2003880@gmail.com>
2026-02-17 22:20:26 -0300

    feat: apk

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -5,4 +5,4 @@ export const API_BASE_URL = __DEV__
       android: "http://10.0.2.2:3000", // Android emulator -> host localhost
       default: "http://192.168.100.91:3000", // Physical devices / iOS -> LAN IP
     })!
-  : "https://your-production-domain.com";
+  : "https://barberia-imperio-py.vercel.app";
